7df673e6b7a68eeb8fa325079931ac6f24d03b07
max <maxown@mail.ru>
2018-07-22 14:05:54 +0300

    Initial commit

---
+++ src/main/java/com/sw/volleyapp/VolleyAppApplication.java
@@ -0,0 +1,12 @@
+package com.sw.volleyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolleyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolleyAppApplication.class, args);
+	}
+}
